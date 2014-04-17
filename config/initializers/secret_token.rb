58db2d79e8f7047dc7bc4647587faced73879aa5
Evan Lloyd <eulloyd@gmail.com>
2014-04-17 19:27:13 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Porfolio::Application.config.secret_key_base = 'eb65f820bf8fce125db1e89f28b7dd3bc6b87a7a7b6f81a8b5f80574fcee30d25690c0bcfeff7fbbf141941e6f2923c5f0963bf96a1900db539240b9170f9812'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Porfolio::Application.config.session_store :cookie_store, key: '_porfolio_session'
