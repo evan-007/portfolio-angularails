e458cf62a546bacb640cd5437e6d7890cb3c3846
Evan Lloyd <eulloyd@gmail.com>
2014-04-19 13:48:58 +0900

    add ui.router dependency

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.services'])
+angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.services', 'ui.router'])
 .config(function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl: '/templates/dashboard.html',
